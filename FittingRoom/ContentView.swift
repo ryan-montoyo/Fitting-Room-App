81d60dd0ec231c363696a2ce0b64cdee8ca3a7b2
ryan-montoyo <ryanmontoyo2003@gmail.com>
2024-06-25 15:43:07 -0400

    Initial Commit

---
+++ FittingRoom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FittingRoom
+//
+//  Created by Ryan Montoyo on 6/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FittingRoom/FittingRoomApp.swift
@@ -0,0 +1,17 @@
+//
+//  FittingRoomApp.swift
+//  FittingRoom
+//
+//  Created by Ryan Montoyo on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct FittingRoomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
